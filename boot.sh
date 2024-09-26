d9ce1bd82820e1e4ffdd43ddd66bba98779bb5cd
Victor is Geek <92975790+victorgeel@users.noreply.github.com>
2024-09-27 04:49:23 +0630

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -5,4 +5,4 @@ touch /cmit_faceswap/logs/access.log
 touch /cmit_faceswap/logs/error.log
 # touch /cmit_faceswap/log/output.log
 
-exec gunicorn -b 0.0.0.0:5000 -w 4 cmit_faceswap:app --access-logfile=/cmit_faceswap/logs/access.log --error-logfile=/cmit_faceswap/logs/error.log --timeout=180
+exec gunicorn -b 0.0.0.0:80 -w 4 cmit_faceswap:app --access-logfile=/cmit_faceswap/logs/access.log --error-logfile=/cmit_faceswap/logs/error.log --timeout=180
